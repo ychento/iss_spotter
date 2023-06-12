9ce77e0bf5d32f5e84165574c849da9f189eb3d5
ychento <your@email.comyiyaochen@gmail.com>
2023-06-12 02:13:57 +0000

    add index2.js

---
+++ index2.js
@@ -0,0 +1,32 @@
+// index2.js
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+// see index.js for printPassTimes 
+// copy it from there, or better yet, moduralize and require it in both files
+
+// Call 
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+
+  nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
+
+  
